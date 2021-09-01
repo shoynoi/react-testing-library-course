cef74874aba1b48fdd40d303f97655cb35de71ca
shoynoi <shoynoi.jp@gmail.com>
2021-09-02 07:46:59 +0900

    API呼び出しのテスト追加

--- src/__tests__/_tdd-01-markup.js
+++ src/__tests__/_tdd-01-markup.js
@@ -1,14 +1,36 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import {Editor} from '../_post-editor-01-markup'
+import {savePost as mockSavePost} from '../api'
+
+jest.mock('../api')
+
+afterAll(() => {
+  jest.clearAllMocks()
+})
 
 test('renders a form with title, content, tags, and a submit button', () => {
-  render(<Editor />)
-  expect(screen.getByLabelText(/title/i)).toBeInTheDocument()
-  expect(screen.getByLabelText(/content/i)).toBeInTheDocument()
-  expect(screen.getByLabelText(/tags/i)).toBeInTheDocument()
+  const fakeUser = {
+    id: 'user-id',
+  }
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+  mockSavePost.mockResolvedValueOnce()
+  render(<Editor user={fakeUser} />)
+  userEvent.type(screen.getByLabelText(/title/i), fakePost.title)
+  userEvent.type(screen.getByLabelText(/content/i), fakePost.content)
+  userEvent.type(screen.getByLabelText(/tags/i), fakePost.tags.join(', '))
   const submitButton = screen.getByRole('button', {name: /submit/i})
   expect(submitButton).toBeInTheDocument()
-  submitButton.click()
+  userEvent.click(submitButton)
   expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
 })
--- src/_post-editor-01-markup.js
+++ src/_post-editor-01-markup.js
@@ -1,21 +1,30 @@
 import * as React from 'react'
+import {savePost} from './api'
 
-function Editor() {
+function Editor({user}) {
   const [isSaving, setIsSaving] = React.useState(false)
   const handleSubmit = (e) => {
     e.preventDefault()
     setIsSaving(true)
+    const {title, content, tags} = e.target.elements
+    const newPost = {
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map((tag) => tag.trim()),
+      authorId: user.id,
+    }
+    return savePost(newPost)
   }
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="input-title">Title</label>
-      <input id="input-title" />
+      <input id="input-title" name="title" />
 
       <label htmlFor="input-content">Content</label>
-      <input id="input-content" />
+      <input id="input-content" name="content" />
 
       <label htmlFor="input-tags">Tags</label>
-      <input id="input-tags" />
+      <input id="input-tags" name="tags" />
 
       <button type="submit" disabled={isSaving}>
         Submit
