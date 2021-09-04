755949988900034c4a93ecea2f1cc59b34d4a139
shoynoi <shoynoi.jp@gmail.com>
2021-09-04 13:51:49 +0900

    renderを再定義して共通部分をまとめる

--- src/__tests__/_react-router-01.js
+++ src/__tests__/_react-router-01.js
@@ -1,27 +1,27 @@
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render as rtlRender, screen} from '@testing-library/react'
 import {BrowserRouter} from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 import {Main} from '../main'
 
+function render(ui, {route = '/', ...renderOptions} = {}) {
+  function Wrapper({children}) {
+    return <BrowserRouter>{children}</BrowserRouter>
+  }
+  window.history.pushState({}, 'Test page', route)
+  return rtlRender(ui, {wrapper: Wrapper, ...renderOptions})
+}
+
 test('main renders about and home and I can navigate to those pages', () => {
-  window.history.pushState({}, 'Test page', '/')
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
+  render(<Main />)
   expect(screen.getByRole('heading')).toHaveTextContent(/home/i)
   userEvent.click(screen.getByText(/about/i))
   expect(screen.getByRole('heading')).toHaveTextContent(/about/i)
 })
 
 test('landing on a bad page shows no match component', () => {
-  window.history.pushState({}, 'Test page', '/something-that-does-not-match')
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
+  render(<Main />, {
+    route: '/something-that-does-not-match',
+  })
   expect(screen.getByRole('heading')).toHaveTextContent(/404/i)
 })
