b5328e4445149e6ecf267124158e9f438b7dd515
shoynoi <shoynoi.jp@gmail.com>
2021-09-04 14:57:12 +0900

    Provider をラップする render を定義して共通部分をまとめる

--- src/__tests__/_redux-01.js
+++ src/__tests__/_redux-01.js
@@ -1,29 +1,33 @@
 import * as React from 'react'
 import {Provider} from 'react-redux'
-import {render, screen} from '@testing-library/react'
+import {render as rtlRender, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {createStore} from 'redux'
-import {store as appStore} from '../redux-store'
 import {Counter} from '../redux-counter'
 import {reducer} from '../redux-reducer'
 
+function render(
+  ui,
+  {
+    initialState,
+    store = createStore(reducer, initialState),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({children}) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return {...rtlRender(ui, {wrapper: Wrapper, ...renderOptions}), store}
+}
+
 test('can render with redux with defaults', () => {
-  render(
-    <Provider store={appStore}>
-      <Counter />
-    </Provider>,
-  )
+  render(<Counter />)
   userEvent.click(screen.getByRole('button', {name: '+'}))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
 })
 
 test('can render with redux with custom initial state', () => {
-  const store = createStore(reducer, {count: '3'})
-  render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>,
-  )
+  render(<Counter />, {initialState: {count: '3'}})
   userEvent.click(screen.getByRole('button', {name: '-'}))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
