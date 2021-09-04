cd21553beecbcd3a4e75ae00ba06d0397a47b8dd
shoynoi <shoynoi.jp@gmail.com>
2021-09-04 14:27:08 +0900

    redux の初期値に関するテストを追加

--- src/__tests__/_redux-01.js
+++ src/__tests__/_redux-01.js
@@ -2,15 +2,28 @@ import * as React from 'react'
 import {Provider} from 'react-redux'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import {store} from '../redux-store'
+import {createStore} from 'redux'
+import {store as appStore} from '../redux-store'
 import {Counter} from '../redux-counter'
+import {reducer} from '../redux-reducer'
 
 test('can render with redux with defaults', () => {
   render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>,
   )
   userEvent.click(screen.getByRole('button', {name: '+'}))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redux with custom initial state', () => {
+  const store = createStore(reducer, {count: '3'})
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+  userEvent.click(screen.getByRole('button', {name: '-'}))
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
