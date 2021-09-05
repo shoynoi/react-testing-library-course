4ee1e2a8c595ff4b0b8328f0c263258126bca036
shoynoi <shoynoi.jp@gmail.com>
2021-09-05 11:33:48 +0900

    findBy を利用することでテストを安定させる

--- src/__tests__/_app-01.js
+++ src/__tests__/_app-01.js
@@ -10,18 +10,20 @@ test('Can fill out a form across multiple pages', async () => {
   mockSubmitForm.mockResolvedValueOnce({success: true})
   const testData = {food: 'test food', drink: 'test drink'}
   render(<App />)
-  userEvent.click(screen.getByRole('link', {name: /fill.*form/i}))
-  userEvent.type(screen.getByLabelText(/food/i), testData.food)
-  userEvent.click(screen.getByRole('link', {name: /next/i}))
-  userEvent.type(screen.getByLabelText(/drink/i), testData.drink)
-  userEvent.click(screen.getByRole('link', {name: /review/i}))
-  expect(screen.getByLabelText(/food/i)).toHaveTextContent(testData.food)
-  expect(screen.getByLabelText(/drink/i)).toHaveTextContent(testData.drink)
-  userEvent.click(screen.getByRole('button', {name: /confirm/i}))
+  userEvent.click(await screen.findByRole('link', {name: /fill.*form/i}))
+  userEvent.type(await screen.findByLabelText(/food/i), testData.food)
+  userEvent.click(await screen.findByRole('link', {name: /next/i}))
+  userEvent.type(await screen.findByLabelText(/drink/i), testData.drink)
+  userEvent.click(await screen.findByRole('link', {name: /review/i}))
+  expect(await screen.findByLabelText(/food/i)).toHaveTextContent(testData.food)
+  expect(await screen.findByLabelText(/drink/i)).toHaveTextContent(
+    testData.drink,
+  )
+  userEvent.click(await screen.findByRole('button', {name: /confirm/i}))
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
   expect(mockSubmitForm).toHaveBeenCalledTimes(1)
   userEvent.click(await screen.findByRole('link', {name: /home/i}))
   expect(
-    screen.getByRole('heading', {level: 1, name: /welcome home/i}),
+    await screen.findByRole('heading', {level: 1, name: /welcome home/i}),
   ).toBeInTheDocument()
 })
